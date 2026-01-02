312b24af13299e75fb41cbfc045aafcad3908369
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-01-02 16:05:48 -0600

    Initial commit: Spring Boot 4.0 project structure

---
+++ src/main/java/com/joelk/countryfilter/CountryfilterApplication.java
@@ -0,0 +1,18 @@
+package com.joelk.countryfilter;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryfilterApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryfilterApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
+}
---
+++ src/test/java/com/joelk/countryfilter/CountryfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelk.countryfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryfilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
